c2344f567516e92d0cf5a840222e8015846b37c0
funfely <javierecordero@outlook.com>
2025-08-26 22:41:29 -0300

    Mi 3er commit tratando de hacer que funcione Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/funfely/CreaTuLanding1Cordero.git,
+  base: 'CreaTuLanding1Cordero',
   plugins: [react()],  
 
 });
